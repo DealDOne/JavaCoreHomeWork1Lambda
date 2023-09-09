92130b05ea1e6fb7b60dba3ed00934ea6f2b0789
DealDOne <vladijsaf@mail.ru>
2023-09-09 14:57:56 +0300

    Initial Commit

---
+++ src/Task1/Calculator.java
@@ -0,0 +1,28 @@
+package Task1;
+
+public class Calculator {
+    @FunctionalInterface
+    public interface Supplier<T>{
+        T get();
+    }
+    static Supplier<Calculator> instance = Calculator::new;
+    @FunctionalInterface
+    public interface BinaryOperator<T>{
+        T apply(T t1, T t2);
+    }
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    @FunctionalInterface
+    public interface UnaryOperator<T>{
+        T apply(T t);
+    }
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    @FunctionalInterface
+    public interface Consumer<T>{
+        void accept(T t);
+    }
+    Consumer<Integer> println = System.out::println;
+}
